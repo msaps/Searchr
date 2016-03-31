b0ceb407199d0285cf3b79fc7f40771cbac7c746
Merrick Sapsford <merrick@sapsford.tech>
2016-03-31 19:37:56 +0100

    Add project

---
+++ Searchr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Searchr
+//
+//  Created by Merrick Sapsford on 31/03/2016.
+//  Copyright © 2016 Merrick Sapsford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
